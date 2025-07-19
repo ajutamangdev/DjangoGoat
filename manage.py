78e7fe43779c9cef10539a649bb4c6f7bbb6aa7d
ajutamang <ajutamangdev@gmail.com>
2025-07-19 08:39:44 +0545

    feat: structure the codebase

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
+
 import os
 import sys
 from pathlib import Path
@@ -7,7 +8,7 @@ from pathlib import Path
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
     sys.path.insert(0, str(Path(__file__).resolve().parent / "src"))
     try:
         from django.core.management import execute_from_command_line
@@ -20,5 +21,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
